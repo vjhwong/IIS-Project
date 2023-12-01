a051d9e9d0fbac251631eddbea966520985c6290
alaia <alaiatorre2001@gmail.com>
2023-12-01 15:37:54 +0100

    trying furhat

--- detect_faces.py
+++ detect_faces.py
@@ -14,27 +14,25 @@ detector = Detector(device="cpu")
 
 cap = cv2.VideoCapture(0)
 recording = False
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    if recording:
-        faces = detector.detect_faces(frame)
-        landmarks = detector.detect_landmarks(frame, faces)
-        aus = detector.detect_aus(frame, landmarks)
-        test_output = model.predict(aus[0])
-        face_idx = 0
-        for (x, y, w, h, p) in faces[0]:
-            cv2.rectangle(frame, (int(x), int(y)), (int(w), int(h)), (0, 0, 255), 3)
-            cv2.putText(frame, test_output[face_idx], (int(x), int(y - 10)), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
-            face_idx+=1
-        cv2.circle(frame, (20,20), 10, (0,0,255), -1)
-    cv2.imshow("frame", frame)
-    key = cv2.waitKey(1)
-    if key == ord(' '):
-        recording = not recording
-    elif key == ord('q'):
-        break
+
+def detect_emotion():
+        ret, frame = cap.read()
+        if not ret:
+            return None
+        if recording:
+            faces = detector.detect_faces(frame)
+            landmarks = detector.detect_landmarks(frame, faces)
+            aus = detector.detect_aus(frame, landmarks)
+            test_output = model.predict(aus[0])
+            face_idx = 0
+            for (x, y, w, h, p) in faces[0]:
+                cv2.rectangle(frame, (int(x), int(y)), (int(w), int(h)), (0, 0, 255), 3)
+                cv2.putText(frame, test_output[face_idx], (int(x), int(y - 10)), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 2)
+                face_idx+=1
+            cv2.circle(frame, (20,20), 10, (0,0,255), -1)
+        cv2.imshow("frame", frame)
+        return test_output
+        
 cap.release()
 cv2.destroyAllWindows()
 
--- interactive_system.py
+++ interactive_system.py
@@ -1,16 +1,31 @@
 from furhat_remote_api import FurhatRemoteAPI
+from detect_faces import detect_emotion 
+import time
+import threading
 
-def main():
+def furhat_interaction():
+    while True:
+        #remember that is an array
+        emotion=detect_emotion()[0]
+
+
+        if emotion == "happy":
+            furhat.say(text="Hello! I'm happy.")
+            # Add more actions specific to happy emotion
+        elif emotion == "sad":
+            furhat.say(text="Hello... I'm feeling a bit down.")
+            # Add more actions specific to sad emotion
+        else:
+            furhat.say(text="Hello! How can I help you today?")
+        time.sleep(5)  # Sleep for 5 seconds
+
+
+if __name__ == "__main__":
     furhat = FurhatRemoteAPI("localhost")
     voices = furhat.get_voices()
     furhat.set_voice(name='Matthew')
 
 
     furhat.say(text="Hi there!")
-
-    
-
-    
-
-if __name__ == "__main__":
-    main()
+    interaction_thread = threading.Thread(target=furhat_interaction)
+    interaction_thread.start()
