6a3e11c580be1f826b4eb38fd209702b0ec8a662
alaia <alaiatorre2001@gmail.com>
2023-12-01 14:17:40 +0100

    aus created

--- emotion-recognition.py
+++ emotion-recognition.py
@@ -19,6 +19,8 @@ dataset = pd.read_csv(path_to_file)
 
 aus_list=[]
 
+emotions = {'0':'neutral','1':'happy','2':'sad','3':'surprise','4':'fear','5':'disgust','6':'angry'}
+
 with open(csv_path, 'w',newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     header_row=['emotion','AU01','AU02','AU04','AU05','AU06','AU07','AU09','AU10','AU11','AU12','AU14','AU15','AU17','AU20','AU23','AU24','AU25','AU26','AU28','AU43']
@@ -35,7 +37,7 @@ with open(csv_path, 'w',newline='') as csv_file:
         else:
             # load the image
             image = cv2.imread(image_path)
-            
+
             
             # analyze the image
             faces = detector.detect_faces(image)
@@ -45,13 +47,15 @@ with open(csv_path, 'w',newline='') as csv_file:
 
 
             #change emotion from number to string with name
-            #if len(faces) != 0:
-            if idx == 247:
-                print(faces)
-            row_data=[expression]+list(aus[0][0])
-            csv_writer.writerow(row_data)
-            idx += 1
-            #else:
-                #print("no face detected")
+            if len(aus[0]) != 0:
+                row_data=[emotions[expression]]+list(aus[0][0])
+                csv_writer.writerow(row_data)
+                idx += 1
+            else:
+                print("no face detected")
+                print(faces[0])
+                print(landmarks[0])
+                print(emotions[0])
+                print(aus[0])
             
 
